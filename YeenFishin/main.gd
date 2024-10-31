60ed90f68fbaba78aa74e356d5c094bb77de7bc4
DarnHyena <60151350+DarnHyena@users.noreply.github.com>
2024-10-31 04:04:15 -0500

    Compatibility Fix
    Fixed compatibility with other custom heads

--- YeenFishin/main.gd
+++ YeenFishin/main.gd
@@ -4,24 +4,19 @@ const ID = "YeenFishin"
 onready var Lure = get_node("/root/SulayreLure")
 
 func _ready():
-	#Vanilla patterns for custom head
+	#Patterns
 	Lure.assign_pattern_texture(ID,"pattern_calico",ID+".species_hyena","mod://Textures/calico_hyena.png")
 	Lure.assign_pattern_texture(ID,"pattern_collie",ID+".species_hyena","mod://Textures/collie_hyena.png")
 	Lure.assign_pattern_texture(ID,"pattern_spotted",ID+".species_hyena","mod://Textures/spot_hyena.png")
 	Lure.assign_pattern_texture(ID,"pattern_tux",ID+".species_hyena","mod://Textures/tux_hyena.png")
+	Lure.assign_pattern_texture(ID, ID+".pattern_hyena", ID+".species_hyena","mod://Textures/hyena_hyena.png")
 	
-	#Custom pattern for vanilla heads
-	Lure.assign_pattern_texture(ID,"pattern_hyena",ID+".species_hyena","mod://Textures/hyena_cat.png")
-	Lure.assign_pattern_texture(ID,"pattern_hyena",ID+".species_hyena","mod://Textures/hyena_dog.png")
-	
-	#Face positioning for custom head
+	#Face positioning and sounds
 	Lure.assign_face_animation(ID,ID+".species_hyena","mod://Species/Animation/hyena_face.tres")
-	
-	#Custom species sounds
 	Lure.assign_species_voice(ID,ID+".species_hyena","mod://Species/Audio/CackleWoop01.wav", "mod://Species/Audio/Snarl001.wav", "mod://Species/Audio/Cackle001.wav" )
 	
 	
-	#Alternative head cosmetics for custom head
+	#Alternative head cosmetics
 	Lure.assign_cosmetic_mesh(ID,"hat_tophat",ID+".species_hyena","mod://Cosmetics/tophat_hyena.tres")
 	Lure.assign_cosmetic_mesh(ID,"accessory_bandaid",ID+".species_hyena","mod://Cosmetics/bandaid_hyena.tres")
 	Lure.assign_cosmetic_mesh(ID,"accessory_cig",ID+".species_hyena","mod://Cosmetics/cig_hyena.tres")
@@ -32,7 +27,7 @@ func _ready():
 	Lure.assign_cosmetic_mesh(ID,"accessory_shades_gold",ID+".species_hyena","mod://Cosmetics/shades_hyena.tres")
 	Lure.assign_cosmetic_mesh(ID,"accessory_shades",ID+".species_hyena","mod://Cosmetics/shades_hyena.tres")
 	
-	#Custom content
+	#Hyena Stuff
+	Lure.add_content(ID,"pattern_hyena","mod://Species/pattern_hyena.tres")
 	Lure.add_content(ID,"species_hyena","mod://Species/species_hyena.tres")
 	Lure.add_content(ID,"tail_hyena","mod://Species/tail_hyena.tres")
-	Lure.add_content(ID,"pattern_hyena","mod://Species/pattern_hyena.tres")
